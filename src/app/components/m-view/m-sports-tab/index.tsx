05fd98e2aeef96aed74ee53f9092efa94e8e14fc
irfanshah35 <irfanshah@universalplus.technology>
2025-11-22 18:38:51 +0500

    sports tabs desing work

--- src/app/components/m-view/m-sports-tab/index.tsx
+++ src/app/components/m-view/m-sports-tab/index.tsx
@@ -1,64 +1,50 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 const MSportsTab = () => {
+    const [activeTab, setActiveTab] = useState(4);
+
+    const items = [
+        { id: 4, text: "Cricket", icon: "icon-cricket-white" },
+        { id: 1, text: "Soccer", icon: "icon-soccer-white" },
+        { id: 2, text: "Tennis", icon: "icon-tennis-white" },
+        { id: 998917, text: "Volleyball", icon: "icon-VolleyBall-white" },
+    ];
+
     return (
-       <div className="w-full">
-      <ul className="flex list-none bg-black mb-0 pl-0 overflow-x-scroll">
-
-        {/* ACTIVE TAB */}
-        <li>
-          <a id="4" className="no-underline">
-            <div
-              className="flex flex-col text-center text-[12px] p-[8px] pb-[3px] min-w-max relative text-white"
-              style={{
-                background: "linear-gradient(-180deg, #f4b501 0%, #f68700 100%)",
-              }}
-            >
-              <div>
-                <div className="flex justify-center mb-[5px]">
-                  <img
-                    src="/assets/transparent-login.gif"
-                    className="w-[20px] h-[20px] sprite icon-cricket-white"
-                  />
-                </div>
-                <div className="text-[12px] font-semibold uppercase">
-                  Cricket
-                </div>
-              </div>
-            </div>
-          </a>
-        </li>
-
-        {/* INACTIVE TABS */}
-        {[
-          { id: 1, text: "Soccer", icon: "icon-soccer-white" },
-          { id: 2, text: "Tennis", icon: "icon-tennis-white" },
-          { id: 998917, text: "Volleyball", icon: "icon-VolleyBall-white" },
-        ].map((item) => (
-          <li key={item.id}>
-            <a  id={`${item.id}`} className="no-underline">
-              <div
-                className="flex flex-col text-center text-[12px] p-[8px] pb-[3px] min-w-max relative bg-black text-white"
-              >
-                <div>
-                  <div className="flex justify-center mb-[5px]">
-                    <img
-                      src="/assets/transparent-login.gif"
-                      className={`w-[20px] h-[20px] sprite ${item.icon}`}
-                    />
-                  </div>
-                  <div className="text-[12px] font-semibold uppercase">
-                    {item.text}
-                  </div>
-                </div>
-              </div>
-            </a>
-          </li>
-        ))}
-
-      </ul>
-    </div>
+        <div className="w-full">
+            <ul className="flex list-none bg-[linear-gradient(180deg,#030a12,#444647_42%,#58595a)] mb-0 pl-0 overflow-x-scroll">
+                {items.map((item) => (
+                    <li key={item.id}>
+                        <a
+                            id={`${item.id}`}
+                            className="no-underline cursor-pointer"
+                            onClick={() => setActiveTab(item.id)}
+                        >
+                            <div
+                                className={`flex flex-col text-center text-[12px] p-2 pb-[3px] min-w-max relative text-white
+                                    ${activeTab === item.id
+                                        ? "bg-[linear-gradient(-180deg,#f4b501_0%,#f68700_100%)]"
+                                        : "bg-transparent"}
+                                `}
+                            >
+                                <div>
+                                    <div className="flex justify-center mb-[5px]">
+                                        <img
+                                            src="/assets/transparent-login.gif"
+                                            className={`w-5 h-5 sprite ${item.icon}`}
+                                        />
+                                    </div>
+                                    <div className="text-[12px] font-semibold uppercase">
+                                        {item.text}
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
