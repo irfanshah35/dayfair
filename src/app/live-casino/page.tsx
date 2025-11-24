95a9b517c8582b8504e9e45aff16735fcd9a24e7
awais <154322092+awaixrao@users.noreply.github.com>
2025-11-24 18:26:38 +0500

    casino route

---
+++ src/app/live-casino/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MLiveCasino from '../components/m-view/m-live-casino'
+
+const LiveCasino = () => {
+  return (
+    <div>
+    <MLiveCasino/> 
+    </div>
+  )
+}
+
+export default LiveCasino
