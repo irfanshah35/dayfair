3f672134d48af260efd8e013df3bce702cdbab23
awais <154322092+awaixrao@users.noreply.github.com>
2025-11-24 17:35:03 +0500

    inplay route and component

---
+++ src/app/inplay/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MInplay from '../components/m-view/m-inplay'
+
+const InplayPage = () => {
+  return (
+    <div>
+      <MInplay/>
+    </div>
+  )
+}
+
+export default InplayPage
