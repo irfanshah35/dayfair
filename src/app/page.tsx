2e4437bc08f368dc03e1483dbbd16d665ef173c8
awais <154322092+awaixrao@users.noreply.github.com>
2025-11-25 14:08:31 +0500

    conditional layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 // import MHeader from "./components/m-view/m-header";
-import MFooter from "./components/m-view/m-footer";
 import MCasino from "./components/m-view/m-live-casino";
-import MMenuTabs from "./components/m-view/m-menu-tabs";
 import MSingleMarket from "./components/m-view/m-single-market";
-import MSportsTab from "./components/m-view/m-sports-tab";
-import MBetSlip from "./components/m-view/m-betslip";
+
 
 
 export default function Home() {
@@ -23,7 +20,6 @@ export default function Home() {
     <div>
       {isMobile ? (
         <>
-         
           {/* <MBetSlip/> */}
           <MSingleMarket />
           <MCasino />
