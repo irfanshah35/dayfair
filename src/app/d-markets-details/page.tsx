56b35e47c1e78b48d8f47fcdb3c9f098f6e4dfd2
Aqeel Anwar <aqeelanwar677@gmail.com>
2025-11-26 02:07:38 +0500

    Add DMarketDetailsPage and RulesModal components for market details view

---
+++ src/app/d-markets-details/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DMarketDetailsPage from '../components/d-view/d-markets-details'
+
+export default function DMarketDetails() {
+  return (
+    <div>
+        <DMarketDetailsPage />
+    </div>
+  )
+}
