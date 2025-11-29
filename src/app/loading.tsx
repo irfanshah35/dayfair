ba5a8974e0b69ea9d29f156da25616ccff844ec8
Aqeel Anwar <aqeelanwar677@gmail.com>
2025-11-29 18:18:21 +0500

    loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Loader from '@/components/common/loader'
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div>
+        <Loader />
+    </div>
+  )
+}
