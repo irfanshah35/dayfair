a4c5029f0737cc5ba84de8ab02fedc71b0c02a24
Aqeel Anwar <aqeelanwar677@gmail.com>
2025-12-10 18:34:20 +0500

    Enhance loading animation and styles for better user experience

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div>
+    <div className='fixed inset-0 flex items-center justify-center bg-white/60 z-9999'>
         <Loader />
     </div>
   )
--- src/components/common/loader/index.tsx
+++ src/components/common/loader/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="flex absolute z-9999999 loderrr inset-0 items-center justify-center min-h-screen bg-white/60">
-      <div className="relative w-12 h-12  animate-spin">
+    <div className="flex absolute z-9999999 loderrr inset-0 items-center justify-center min-h-screen bg-white/10">
+      <div className="relative w-20 h-20  animate-spin">
         {[...Array(8)].map((_, index) => (
           <div
             key={index}
-            className="absolute w-1.5 h-1.5 bg-black rounded-full"
+            className="absolute w-[6.5px] h-[6.5px] bg-black rounded-full"
             style={{
               top: "50%",
               left: "50%",
-              transform: `rotate(${index * 45}deg) translate(0, -16px)`,
+              transform: `rotate(${index * 45}deg) translate(0, -18px)`,
               transformOrigin: "0 0",
               opacity: index === 0 ? 0 : 1,
             }}
