9fd94e8fd36d18ded882033f0a22b267b0451888
Aqeel Anwar <aqeelanwar677@gmail.com>
2025-12-22 01:32:45 +0500

    resolbing issues

---
+++ src/app/(authenticated)/rules/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Rules() {
+    return (
+        <>
+            <div className="mb-[5px] p-1.5 bg-[#2c3f50] text-white leading-[22px] text-[18px] font-medium">
+                <h4 className="mb-0 relative top-[-1px]">Rules</h4>
+            </div>
+        </>
+    )
+}
