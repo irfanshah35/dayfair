4bb50da1f05dde557201f3f08814cad99a5ce81d
awais <154322092+awaixrao@users.noreply.github.com>
2025-12-21 12:46:44 +0500

    scrollbar

--- src/components/m-view/m-single-market/index.tsx
+++ src/components/m-view/m-single-market/index.tsx
@@ -57,7 +57,7 @@ const MSingleMarket = () => {
 
       {/* =================== UI SAME NO CHANGE =================== */}
       <div className="lg:px-[9px]">
-        <div className="lg:hidden overflow-y-auto no-scrollbar max-h-[265px]">
+        <div className="lg:hidden overflow-y-auto  max-h-[265px]">
           {filteredEvents.length > 0 ? (
             filteredEvents?.map((item, idx) => (
               <div
